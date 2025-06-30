8d25630ee53ec963f79e833645ee33e983ef375c
Abrar Yeasir <iamabraryeasir@gmail.com>
2025-06-30 19:02:22 +0600

    Add SumMethod class for basic addition functionality

---
+++ src/methods_07/SumMethod.java
@@ -0,0 +1,12 @@
+package methods_07;
+
+public class SumMethod {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("This is the main Method");
+        System.out.println(add(26, 56));
+    }
+}
